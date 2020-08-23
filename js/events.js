e69f70bafed9ec2d5cd34af4d3df88dd4bffd2c5
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-23 19:35:40 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 
 function pressIt() {
   $(document).on('keydown', function(key) {
-    if(key.which==103) {
+    if(key.which==71) {
       alert('g was pressed')
     }
 })
