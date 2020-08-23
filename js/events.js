849c01c6ed5f61519f8d73a1fb442e31e2bcb3ac
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-23 19:32:32 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,12 +11,8 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function (key) {
-    if (key.which==103){
-      alert('g was pressed')
-    }
-  })
-}
+  $('form').on('keydown', function () {
+    
 
 
 
